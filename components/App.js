5458b3dbaf0ec3b692a2eb894233ba26205a64f1
Kai Romero <jorge.romero.ortega@gmail.com>
2016-12-30 18:16:14 +0100

    otro mensaje

--- components/App.js
+++ components/App.js
@@ -6,19 +6,6 @@ export default class App extends Component {
     this.state = { counter: 0 };
   }
 
-  componentDidMount() {
-    this.interval = setInterval(
-      this.increment.bind(this),
-      1000
-    )
-  }
-
-  increment() {
-    this.setState(({ counter }) => {
-      return {counter: counter + 1};
-    });
-  }
-
   componentWillUnmount() {
     clearInterval(this.interval)
   }
